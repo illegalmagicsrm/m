47bbdeb3405eded33255fac4f9b20e834c6afaa7
illegalmagicsrm <illegalmagic.srm@gmail.com>
2025-07-26 15:05:54 +0600

    Fix JSX structure error in ProductsPage

--- src/components/ProductsPage.tsx
+++ src/components/ProductsPage.tsx
@@ -281,7 +281,6 @@ export default function ProductsPage({ onNavigate }: ProductsPageProps) {
                   Clear Filters
                 </button>
               </div>
-            )}
           </div>
         </div>
       </div>
